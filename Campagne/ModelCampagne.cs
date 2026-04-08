bcacab98f841c5d32c34110ff633c5a800830286
BARZILAI, Shoshanah <s.barzilai@tf1.onmicrosoft.com>
2026-04-08 15:39:14 +0200

    récupération données json (Ids)

---
+++ Campagne/ModelCampagne.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace GestionOI.Campagne;
+
+public class ModelCampagne
+{
+    public static void DataContent()
+    {
+        var json = File.ReadAllText("../../../Data/campagnes.json");
+        //return new StringContent(jsonString, Encoding.UTF8);
+        var data = JsonSerializer.Deserialize<List<Campagne>>(json);
+
+        if (data != null)
+            foreach (var campagne in data)
+            {
+                Console.WriteLine($"Id de la campagne: {campagne.Id}");
+            }
+    }
+    public class Campagne {
+        public int Id {  get; set; } 
+        public double Budget { get; set; }
+        public string? Acheteur { get; set; }
+        public string? Commentaire { get; set; }
+            
+     };
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GestionOI.Campagne;
+
+//ModelCampagne.AfficherCampagne();
+ModelCampagne.DataContent();
